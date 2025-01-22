cb5aeb667ff07272c6bb5deddbe89b6adf193f83
Martin <saadv89@gmail.com>
2025-01-22 14:50:48 +0100

    front page design finished

--- fetchPostService.js
+++ fetchPostService.js
@@ -1,7 +1,6 @@
 export async function fetchPost() {
     try {
-        const response = await fetch('http://localhost:5049/api/postgrespost/getpost?id=1');
-        if (!response.ok) {
+        const response = await fetch('http://localhost:5049/api/postgrespost/getpost?id=1'); if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
         const post = await response.json();
@@ -14,8 +13,7 @@ export async function fetchPost() {
 
 export async function fetchAllPosts() {
     try {
-        const response = await fetch('http://localhost:5049/api/postgrespost/getallposts');
-        if (!response.ok) {
+        const response = await fetch('http://localhost:5049/api/postgrespost/getallposts'); if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
         const posts = await response.json();
@@ -25,3 +23,13 @@ export async function fetchAllPosts() {
         console.error('There has been a problem with your fetch operation:', error);
     }
 }
+
+/*
+id
+rating
+title
+submitted
+username
+subReddit
+commentAmount
+*/
