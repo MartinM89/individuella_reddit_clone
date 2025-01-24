6cc8b76849e8e01300e3abff55df85efe1d47aac
Martin <saadv89@gmail.com>
2025-01-24 22:04:33 +0100

    added header nav to create post and post page

---
+++ javascript/printHeaderNav.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const headerNavContainer = document.querySelector('#header-nav');
+
+    if (headerNavContainer) {
+        headerNavContainer.innerHTML = `
+            <img src="../images/reddit_logo.svg" alt="">
+            <a id="logo-options" href="">hot</a>
+            <a href="">new</a>
+            <a href="">rising</a>
+            <a href="">controversial</a>
+            <a href="">top</a>
+            <a href="">wiki</a>
+            <div id="sign-up-div">
+                <p>Want to join? <span>Log in</span> or <span class="sign-up-button">sign up</span> in seconds.</p>
+            </div>
+        `;
+    }
+});
