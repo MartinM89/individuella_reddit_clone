d8c574680671b4335ed92e28927d170823316fe9
Martin <saadv89@gmail.com>
2025-01-24 19:12:31 +0100

    cleaned up css code for navbar

--- javascript/populateNavbar.js
+++ javascript/populateNavbar.js
@@ -4,17 +4,26 @@ document.addEventListener("DOMContentLoaded", async function () {
     const subReddits = await fetchSubReddits();
     const subRedditContainer = document.getElementById('top-subreddits');
 
-    if (subReddits) {
-        subReddits.forEach(subReddit => {
-            const subRedditDiv = document.createElement('div');
-            subRedditDiv.classList.add('post-div');
+    const subRedditsLength = subReddits.length;
+    let subRedditsPrints = 1;
 
+    subReddits.forEach(subReddit => {
+        const subRedditDiv = document.createElement('div');
+        subRedditDiv.classList.add('top-subreddits');
+
+        if (subRedditsPrints < subRedditsLength) {
             subRedditDiv.innerHTML = `
-                <a class="top-bar-links" href="">${subReddit.name}</a>
-                <p> &ndash; </p>
+                <a href="">${subReddit.name}</a>
+                <p>&ndash;</p>
             `;
+        } else {
+            subRedditDiv.innerHTML = `
+                <a href="">${subReddit.name}</a>
+            `;
+        }
 
-            subRedditContainer.appendChild(subRedditDiv);
-        });
-    }
+        subRedditContainer.appendChild(subRedditDiv);
+        subRedditsPrints++;
+    });
+
 });
